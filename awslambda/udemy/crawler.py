8af55e23fb3fc86703dedb87cac27a2161ed348d
Nabeel Ahmad Khan <nabeelahmadkh@gmail.com>
2019-02-10 20:14:41 -0600

    udemy lambda updated

--- awslambda/udemy/crawler.py
+++ awslambda/udemy/crawler.py
@@ -1,4 +1,4 @@
-# Crawling web page to find certain parameters
+# Crawling web page to fetch current data
 from lxml import html
 import requests
 from datetime import datetime
@@ -6,7 +6,9 @@ import time
 
 def crawl(url):
     try:
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:20.0) Gecko/20100101 Firefox/20.0'}
+        # Fake Header
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:20.0) '\
+        'Gecko/20100101 Firefox/20.0'}
         page = requests.get(url, headers=headers)
         tree = html.fromstring(page.content)
     except Exception as e:
@@ -15,60 +17,35 @@ def crawl(url):
                     "price" : None,
                     "duration" : None,
                     "last_update" : None,
-                    "category" : None
+                    "category" : None,
+                    "description" : None
                 }
 
-    
-    # with open('page_content.html', 'w') as fid:
-    #     fid.write(page.content)
-
-    # with open('page_content.html', 'r') as f:
-    #     page = f.read()
-    # tree = html.fromstring(page)
-
-    # print("current page is ", tree)
+    # Fetching Data from Crawled Page
     current_price = tree.xpath('//span[@class="price-text__current"]/text()')
     duration = tree.xpath('//span[@class="curriculum-header-length"]/text()')
     last_updated = tree.xpath('//div[@data-purpose="last-update-date"]/text()')
     category = tree.xpath('//meta[@property="udemy_com:category"]/@content')
-    if current_price == []:
-        current_price = None
-        price = None
-    else:
-        price = float(current_price[1].strip()[1:])
-    
-    if duration == []:
-        duration = None
-        course_duration = None
-    else:
-        course_duration = duration[0].strip().split(":")[0]
-    
-    if last_updated == []:
-        last_updated = None
-        course_start_date = None
-    else:
-        course_last_updated = last_updated[0].strip()[13:]
-        course_start_date = datetime.strptime(course_last_updated, "%m/%Y")
-        course_start_date = "{}-{}-{}".format(course_start_date.year, course_start_date.month, course_start_date.day)
-    
-    if category == []:
-        category = None
-        course_category = []
-    else:
-        course_category = [category[0]]
+    description = tree.xpath('//meta[@name="description"]/@content')
 
+    current_price = None if current_price == [] else float(current_price[1].strip()[1:])
+    duration = None if duration == [] else duration[0].strip().split(":")[0]
+    category = None if category == [] else [category[0]]
+    description = None if description == [] else description[0]
+        
+    last_updated = None if last_updated == [] else \
+        "{}-{}-{}".format(
+                            datetime.strptime(last_updated[0].strip()[13:], "%m/%Y").year, 
+                            datetime.strptime(last_updated[0].strip()[13:], "%m/%Y").month, 
+                            datetime.strptime(last_updated[0].strip()[13:], "%m/%Y").day
+                        )
     
-
-    # print("\n current price is : ", price)
-    # print("\n duration is ", course_duration)
-    # print("\n last_update is ", course_start_date)
-    # print("\n category is ", category[0])
-
     return {
-        "price" : price,
-        "duration" : course_duration,
-        "last_update" : course_start_date,
-        "category" : course_category
+        "price" : current_price,
+        "duration" : duration,
+        "last_update" : last_updated,
+        "category" : category,
+        "description" : description
     }
 
 # time1 = time.time()
