16c1e008ffc278949354efc9e6f7d5c6d63ce48d
Ali Asgher <ali-asgher@uiowa.edu>
2019-02-14 14:07:14 -0600

    Added support for utility functions in lambdas

--- awslambda/open_learning/open-learning.py
+++ awslambda/open_learning/open-learning.py
@@ -2,6 +2,7 @@ import nltk
 import time
 import requests, json
 from datetime import datetime
+from awslambda.utils import search_data_elastic_search, add_data_elastic_search
 
 def is_english(name):
     for word in nltk.wordpunct_tokenize(name):
@@ -9,45 +10,47 @@ def is_english(name):
             return False
     return True
 
+
 def generate_key(url):
     c = ''.join([str(ord(c)) for c in url if c.isalpha()])
     c = int(c) % (2 ** 32 - 1)
     return str(c)
+
 
 def main():
     URL = 'https://www.openlearning.com/api/courses/list?type=free,paid'
     SEARCH_URL = 'https://search-coursehub-mmrw23yio2a4ylx2cgmx34eiyy.us-east-2.es.amazonaws.com/courses/_search'
     POST_URL = 'https://search-coursehub-mmrw23yio2a4ylx2cgmx34eiyy.us-east-2.es.amazonaws.com/courses/course'
 
+
+    count = 1
     response = requests.get(URL)
     courses = response.json()['courses']
     headers = {'Content-type': 'application/json'}
     start = time.time()
-    count = 1
     total = len(courses)
+
     for course in courses:
         if is_english(course['name']):
+
             key = PROVIDER + '-' + generate_key(course['courseUrl'])
-            res = requests.post(SEARCH_URL, data=str(json.dumps({"query" : {"term" : { "CourseId" : key }}})), headers=headers)
-            if json.loads(res.text)['hits']['total'] > 0:
+            if search_data_elastic_search(SEARCH_URL, { "CourseId" : key }):
                 print('[$] Already Processed', count, '/', total)
                 count += 1
                 continue
-            print('[#] Processing', course['name'], count, '/', total)
+
             instructors = [{"InstructorId": str(generate_key(course['creator']['profileUrl'])), "InstructorName": course['creator']['fullName'],
                             "ProfilePic": course['creator']['imageUrl']}]
 
-            data = str(json.dumps({"CourseId":key, "CourseProvider": PROVIDER, "Title": course["name"], "Category":course["category"],
+            data = {"CourseId":key, "CourseProvider": PROVIDER, "Title": course["name"], "Category":course["category"],
                             "CourseDuration": int(course["duration"].split(' ')[0]), "Paid": True if course["type"] == "paid" else False,
                             "Price": int(str(course["price"]).split(" ")[0]), "PriceCurrency": "USD", "Instructors": instructors,
                             "URL": course["courseUrl"], "Rating": 0, "Description": course["summary"], "CourseImage": course["image"],
                             "StartDate": datetime.strptime(course["startDate"], '%d %b %Y').isoformat().split('T')[0] if "startDate" in course and course["startDate"] is not None else None,
                             "EndDate": datetime.strptime(course["endDate"], '%d %b %Y').isoformat().split('T')[0] if "endDate" in course and course["endDate"] is not None else None,
-                            "SelfPaced": course["selfPaced"]}))
+                            "SelfPaced": course["selfPaced"]}
 
-            database_response = requests.post(POST_URL, headers=headers, data=data)
-            if database_response.status_code != 201:
-                print(key, database_response.status_code)
+            add_data_elastic_search(POST_URL, data)
         else:
             print('[X] Course text not in english', count, '/', total)
         count += 1
