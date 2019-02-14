db0b244b685ba0be2b6b391a1714a04f62c967bf
Ali Asgher <ali-asgher@uiowa.edu>
2019-02-14 14:28:11 -0600

    Test: Wrote test to test requests for course data fetch from website

--- awslambda/test/unit-test.py
+++ awslambda/test/unit-test.py
@@ -1,10 +1,11 @@
 import unittest
 import requests_mock
 
-from awslambda.utils import add_data_elastic_search, search_data_elastic_search
+from awslambda.utils import add_data_elastic_search, search_data_elastic_search, fetch_data
 
 SEARCH_URL = 'https://search-coursehub-mmrw23yio2a4ylx2cgmx34eiyy.us-east-2.es.amazonaws.com/courses/_search'
 DATA_URL = 'https://search-coursehub-mmrw23yio2a4ylx2cgmx34eiyy.us-east-2.es.amazonaws.com/courses/course'
+URL = 'https://iversity.org/api/v1/courses'
 
 class TestLambdas(unittest.TestCase):
 
@@ -38,6 +39,21 @@ class TestLambdas(unittest.TestCase):
             mocker.register_uri('POST', requests_mock.ANY, json=content, status_code=201)
             output = search_data_elastic_search(SEARCH_URL, {})
             self.assertEqual(output, False)
+
+
+    def test_fetch_course_data_negative_response(self):
+        with requests_mock.Mocker() as mocker:
+            mocker.register_uri('GET', requests_mock.ANY, json={}, status_code=201)
+            output = fetch_data(URL, 2, 1)
+            self.assertEqual(output, False)
+
+    def test_fetch_course_data_positive_response(self):
+        with requests_mock.Mocker() as mocker:
+            content = {'courses': []}
+            mocker.register_uri('GET', requests_mock.ANY, json=content, status_code=201)
+            output = fetch_data(URL, 2, 1)
+            self.assertEqual(output, [])
+
 
 
 if __name__ == '__main__':
