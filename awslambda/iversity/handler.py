c8fc39ae0aa6078f505f3efe1568f2ddbb065ef9
Ali Asgher <ali-asgher@uiowa.edu>
2019-02-17 23:26:32 -0600

    Feat: Made configuration changes

--- awslambda/iversity/handler.py
+++ awslambda/iversity/handler.py
@@ -1,3 +1,4 @@
+import json
 import time
 from os import sys, path
 from datetime import datetime
@@ -8,9 +9,10 @@ import utils
 
 def lambda_handler(event, context):
     PROVIDER = 'Iversity'
-    URL = 'https://iversity.org/api/v1/courses'
-    POST_URL = 'https://search-coursehub-mmrw23yio2a4ylx2cgmx34eiyy.us-east-2.es.amazonaws.com/courses/course',
-    SEARCH_URL = 'https://search-coursehub-mmrw23yio2a4ylx2cgmx34eiyy.us-east-2.es.amazonaws.com/courses/_search'
+
+    URL = "https://iversity.org/api/v1/courses"
+    POST_URL = "https://search-coursehub-mmrw23yio2a4ylx2cgmx34eiyy.us-east-2.es.amazonaws.com/courses/course"
+    SEARCH_URL = "https://search-coursehub-mmrw23yio2a4ylx2cgmx34eiyy.us-east-2.es.amazonaws.com/courses/_search"
 
     courses = utils.fetch_data(URL, 5, 30)
     if courses != False:
@@ -22,31 +24,33 @@ def lambda_handler(event, context):
         for course in courses:
             try:
                 if 'Eng' in course['language']:
-                    key = PROVIDER + '-' + str(course['id'])
+                    key = PROVIDER + '-'
+                    key += str(course['id']) if 'id' in course else course['url']
 
                     if utils.search_data_elastic_search(SEARCH_URL, {"CourseId": key}):
                         print('[$] Already Processed', count, '/', total)
                         count += 1
                         continue
 
-                    instructors = [{"InstructorId": ins['id'], "InstructorName": ins['name'], "ProfilePic": ins['image']}
+                    instructors = [{"InstructorId": ins['id'] if 'id' in ins else None,
+                                    "InstructorName": ins['name'] if 'name' in ins else None,
+                                    "ProfilePic": ins['image'] if 'image' in ins else None}
                                    for ins in course['instructors']] if 'instructors' in course else None
 
-                    data = ({"CourseId": key, "CourseProvider": PROVIDER, "Title": course["title"],
-                             "Category": course["discipline"],
-                             "CourseDuration": int(course["duration"]) if len(course["duration"]) != 0 else None,
-                             "Paid": True if course["verifications"][0]['price'] is not None else False,
-                             "Price": int(course["verifications"][0]['price'].split(" ")[0]) if course["verifications"][0][
-                                                                                                    "price"] is not None else None,
+                    data = ({"CourseId": key, "CourseProvider": PROVIDER,
+                             "Title": course["title"] if 'title' in course else None,
+                             "Category": course["discipline"] if 'discipline' in course else None,
+                             "CourseDuration": int(course["duration"]) if 'duration' in course and len(course["duration"]) != 0 else None,
+                             "Paid": True if 'verifications' in course and course["verifications"][0]['price'] is not None else False,
+                             "Price": int(course["verifications"][0]['price'].split(" ")[0]) if 'verifications' in course and course["verifications"][0]["price"] is not None else None,
                              "PriceCurrency": "Euro", "Instructors": instructors,
-                             "URL": course["url"], "Rating": 0, "Description": course["description"],
-                             "CourseImage": course["image"],
-                             "StartDate":
-                                 datetime.strptime(course["start_date"].split('T')[0], '%Y-%m-%d').isoformat().split('T')[
-                                     0] if "start_date" in course and course["start_date"] is not None else None,
-                             "EndDate":
-                                 datetime.strptime(course["end_date"].split('T')[0], '%Y-%m-%d').isoformat().split('T')[
-                                     0] if "end_date" in course and course["end_date"] is not None else None,
+                             "URL": course["url"] if 'url' in course else None,
+                             "Rating": 0, "Description": course["description"] if 'description' in course else None,
+                             "CourseImage": course["image"] if "image" in course else None,
+                             "StartDate": datetime.strptime(course["start_date"].split('T')[0], '%Y-%m-%d').isoformat().split('T')[0]
+                             if "start_date" in course and course["start_date"] is not None else None,
+                             "EndDate":datetime.strptime(course["end_date"].split('T')[0], '%Y-%m-%d').isoformat().split('T')[0]
+                             if "end_date" in course and course["end_date"] is not None else None,
                              "SelfPaced": None})
 
                     utils.add_data_elastic_search(POST_URL, data)
@@ -63,3 +67,6 @@ def lambda_handler(event, context):
         print("Total Courses Added:", add_count)
         print('Total time taken:', round(time.time() - start, 2), 'sec\n')
         return True
+
+if __name__ == '__main__':
+    lambda_handler('', '')
--- awslambda/open_learning/handler.py
+++ awslambda/open_learning/handler.py
@@ -1,3 +1,4 @@
+import json
 import time
 import nltk
 from os import sys, path
@@ -14,9 +15,10 @@ def lambda_handler(event, context):
 
     PROVIDER = 'Open Learning'
     words_set = set(nltk.corpus.words.words())
-    URL = 'https://www.openlearning.com/api/courses/list?type=free,paid'
-    POST_URL = 'https://search-coursehub-mmrw23yio2a4ylx2cgmx34eiyy.us-east-2.es.amazonaws.com/courses/course',
-    SEARCH_URL = 'https://search-coursehub-mmrw23yio2a4ylx2cgmx34eiyy.us-east-2.es.amazonaws.com/courses/_search'
+
+    URL = "https://www.openlearning.com/api/courses/list?type=free,paid"
+    POST_URL = "https://search-coursehub-mmrw23yio2a4ylx2cgmx34eiyy.us-east-2.es.amazonaws.com/courses/course"
+    SEARCH_URL = "https://search-coursehub-mmrw23yio2a4ylx2cgmx34eiyy.us-east-2.es.amazonaws.com/courses/_search"
 
     courses = utils.fetch_data(URL, 5, 30)
     if courses != False:
@@ -34,22 +36,24 @@ def lambda_handler(event, context):
                         continue
 
                     instructors = [{"InstructorId": str(utils.generate_key(course['creator']['profileUrl'])),
-                                    "InstructorName": course['creator']['fullName'],
-                                    "ProfilePic": course['creator']['imageUrl']}]  if 'creator' in course else None
+                                    "InstructorName": course['creator']['fullName'] if 'fullName' in course['creator'] else None,
+                                    "ProfilePic": course['creator']['imageUrl'] if 'imageUrl' in course['creator'] else None}]  if 'creator' in course else None
 
-                    data = {"CourseId": key, "CourseProvider": PROVIDER, "Title": course["name"],
-                            "Category": course["category"],
-                            "CourseDuration": int(course["duration"].split(' ')[0]),
-                            "Paid": True if course["type"] == "paid" else False,
-                            "Price": int(str(course["price"]).split(" ")[0]), "PriceCurrency": "USD",
-                            "Instructors": instructors,
-                            "URL": course["courseUrl"], "Rating": 0, "Description": course["summary"],
-                            "CourseImage": course["image"],
+                    data = {"CourseId": key, "CourseProvider": PROVIDER,
+                            "Title": course["name"] if 'name' in course else None,
+                            "Category": course["category"] if 'category' in course else None,
+                            "CourseDuration": int(course["duration"].split(' ')[0]) if 'duration' in course else None,
+                            "Paid": True if 'type' in course and course["type"] == "paid" else False,
+                            "Price": int(str(course["price"]).split(" ")[0]) if 'price' in course else None,
+                            "PriceCurrency": "USD", "Instructors": instructors,
+                            "URL": course["courseUrl"], "Rating": 0,
+                            "Description": course["summary"] if 'summary' in course else None,
+                            "CourseImage": course["image"] ,
                             "StartDate": datetime.strptime(course["startDate"], '%d %b %Y').isoformat().split('T')[
                                 0] if "startDate" in course and course["startDate"] is not None else None,
                             "EndDate": datetime.strptime(course["endDate"], '%d %b %Y').isoformat().split('T')[
                                 0] if "endDate" in course and course["endDate"] is not None else None,
-                            "SelfPaced": course["selfPaced"]}
+                            "SelfPaced": course["selfPaced"] if 'selfPaced' in course else None}
 
                     utils.add_data_elastic_search(POST_URL, data)
                 except Exception as e:
@@ -59,3 +63,7 @@ def lambda_handler(event, context):
             count += 1
 
         print('Total time taken:', round(time.time() - start, 2), 'sec\n')
+
+
+if __name__ == '__main__':
+    lambda_handler('', '')
