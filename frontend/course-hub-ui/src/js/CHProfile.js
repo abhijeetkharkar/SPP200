a2cb02d41292faa501f2dad906a0936025f7c36b
Ali Asgher <ali-asgher@uiowa.edu>
2019-02-25 15:15:44 -0600

    Created an empty frontend template for user profile

---
+++ frontend/course-hub-ui/src/js/CHProfile.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class ProfilePage extends Component{
+    constructor(props, context) {
+        console.log("CHProfile Constructor");
+        super(props, context);
+    }
+
+    render() {
+        return (
+            <div>
+                Hello
+            </div>
+        );
+    }
+}
+
+export default ProfilePage
