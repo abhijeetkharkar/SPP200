55857133a32ac525f397657e73fb31de4f288d35
Nabeel Ahmad Khan <nabeelahmadkh@gmail.com>
2019-02-28 02:20:27 -0600

    elastic search updated in signup

--- frontend/course-hub-ui/src/initializeFirebase.js
+++ frontend/course-hub-ui/src/initializeFirebase.js
@@ -1,4 +1,6 @@
 import firebase from "firebase";
+import 'firebase/auth';
+import app from 'firebase/app'
 
 const firebaseInitialization = firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_KEY,
@@ -8,5 +10,28 @@ const firebaseInitialization = firebase.initializeApp({
   /* storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID */
 });
+
+// class Firebase {
+//   constructor() {
+//     app.initializeApp(firebaseInitialization);
+
+//     this.auth = app.auth();
+//   }
+
+//   // *** Auth API ***
+
+//   doCreateUserWithEmailAndPassword = (email, password) =>
+//     this.auth.createUserWithEmailAndPassword(email, password);
+
+//   doSignInWithEmailAndPassword = (email, password) =>
+//     this.auth.signInWithEmailAndPassword(email, password);
+
+//   doSignOut = () => this.auth.signOut();
+
+//   doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+//   doPasswordUpdate = password =>
+//     this.auth.currentUser.updatePassword(password);
+// }
 
 export default firebaseInitialization;
