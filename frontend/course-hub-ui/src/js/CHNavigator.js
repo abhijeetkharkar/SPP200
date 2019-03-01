5a2b4953b9f4c2cc13b4964c72b91a380ef08986
Ali Asgher <ali-asgher@uiowa.edu>
2019-03-01 15:03:31 -0600

    Frontend: Added profile router in navigator

--- frontend/course-hub-ui/src/js/CHNavigator.js
+++ frontend/course-hub-ui/src/js/CHNavigator.js
@@ -2,31 +2,57 @@ import React, { Component } from 'react';
 import '../css/bootstrap.min.css';
 import '../css/common-components.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import ProfilePage from "./CHProfile";
+import { Button, Dropdown } from 'react-bootstrap';
+import {doSignOut} from '../FirebaseUtils';
 
 class CHNavigator extends Component {
 
+    constructor(props, context) {
+        super(props, context);
+        this.handleSignOut = this.handleSignOut.bind(this);
+        this.handleViewProfile = this.handleViewProfile.bind(this);
+    }
+
+    handleSignOut = e => {
+        const self = this;
+        doSignOut().then( () => {
+            self.props.updateContent("home", null, null, null);
+        }).catch(function (error) {
+            console.log(error);
+        });
+    }
+
+    handleViewProfile = e => {
+        this.props.updateContent("profile", null, null, null);
+    }
+
     render() {
-        // const self = this;
-
         return (
-            <Router>
-                <div id="homeHeader">
-                    {/* <div id="website-name-container"> */}
-                    <h1 className="website-name">Course-Hub</h1>
-                    {/* </div> */}
-                    <div id="website-navigators-container">
-                        <button className="my-nav-tabs" onClick={(e) => this.props.updateContent("loginScreen",null, null, null)}><FontAwesomeIcon icon="sign-in-alt" /> Login/Signup</button>
-                        <button className="my-nav-tabs">Deals</button>
-                        <Link to="/profile"><button className="my-nav-tabs" onClick={(e) => this.props.updateContent("profileScreen",null, null, null)}>Profile</button></Link>
-                    </div>
+            <div id="homeHeader">
+                <h1 className="website-name">Course-Hub</h1>
+                <div id="website-navigators-container">
+                    {!this.props.signedIn &&
+                        <Button className="my-nav-tabs" onClick={(e) => this.props.updateContent("loginScreen", null, null, null)}>
+                            <FontAwesomeIcon icon="sign-in-alt" />
+                            &nbsp;&nbsp;Login/Signup
+                        </Button>
+                    }
 
-                    <Switch>
-                        <Route exact path="/profile" component={ProfilePage} />
-                    </Switch>
+                    {this.props.signedIn &&
+                        <Dropdown>
+                            <Dropdown.Toggle className="my-nav-tabs" id="signedInOptions">
+                                Hello, {this.props.firstName?this.props.firstName:"Jon Snow"}&nbsp;&nbsp;
+                            </Dropdown.Toggle>
+
+                            <Dropdown.Menu>
+                                <Dropdown.Item onClick={this.handleViewProfile}>View Profile</Dropdown.Item>
+                                <Dropdown.Item onClick={this.handleSignOut}>Logout</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    }
+                    <Button className="my-nav-tabs">Deals</Button>
                 </div>
-            </Router>
+            </div>
         );
     }
 }
