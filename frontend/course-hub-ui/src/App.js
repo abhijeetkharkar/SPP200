594f2e032d7c0c377b1af080de019a42d122fd5f
Ali Asgher <ali-asgher@uiowa.edu>
2019-03-01 15:04:00 -0600

    Frontend: Added profile page option in app.js

--- frontend/course-hub-ui/src/App.js
+++ frontend/course-hub-ui/src/App.js
@@ -1,36 +1,54 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom'
 import './App.css';
 import './css/bootstrap.min.css';
 import CHNavigator from './js/CHNavigator'
 import CHLandingContent from './js/CHLandingContent';
 import LoginPage from './js/CHLogin';
 import SignupPage from './js/CHSignup';
-import CHFilters from './js/CHFilters';
-import CHRightLane from './js/CHRightLane';
 import CHFooter from './js/CHFooter';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faEnvelope, faKey, faSignInAlt, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faKey, faSignInAlt, faSearch, faAngleDown } from '@fortawesome/free-solid-svg-icons';
 import { faFacebookF, faGithub, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import firebaseInitialization, {isUserSignedIn} from './FirebaseUtils';
+import app from 'firebase/app';
 import ProfilePage from "./js/CHProfile";
+import ProfileNavigator from "./js/CHProfileNavigator";
 
-library.add(faEnvelope, faKey, faFacebookF, faGithub, faTwitter, faLinkedin, faSignInAlt, faSearch);
-
+library.add(faEnvelope, faKey, faFacebookF, faGithub, faTwitter, faLinkedin, faSignInAlt, faSearch, faAngleDown);
 
 class App extends Component {
 
   constructor() {
     super();
     this.state = {
-      choice: "home",
       optional1: "",
       optional2: "",
       optional3: ""
     };
+
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick = (choice, optional1, optional2, optional3) => { this.setState({ choice }); this.setState({ optional1 }); this.setState({ optional2 }); this.setState({ optional3 }) }
+  componentWillMount() {
+    firebaseInitialization.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({
+          choice: "homeSignedIn"
+        });
+      } else {
+        this.setState({
+          choice: "home"
+        });
+      }
+    });
+  }
+
+  handleClick = (choice, optional1, optional2, optional3) => {
+    this.setState({ choice });
+    this.setState({ optional1 });
+    this.setState({ optional2 });
+    this.setState({ optional3 });
+  }
 
   render() {
     const choice = this.state.choice
@@ -39,17 +57,50 @@ class App extends Component {
     const optional3 = this.state.optional3
     return (
       <div className="App container-fluid">
-        { choice === "home" && 
-          [<CHNavigator updateContent={this.handleClick} />,
-          <div className="container-landing my-content-landing">
+        {choice === "home" &&
+          [<CHNavigator updateContent={this.handleClick} signedIn={false} key="keyNavigatorLandingContent"/>,
+          <div className="container-landing my-content-landing"  key="keyLandingContent">
             <CHLandingContent />
           </div>,
-          <CHFooter />]
+          <CHFooter key="keyFooterLandingContent"/>]
         }
 
-        { choice === "loginScreen" && <LoginPage updateContent = {this.handleClick}/> }
-        { choice === "signupScreen" && <SignupPage updateContent = {this.handleClick}/> }
-        { choice === "profileScreen" && <ProfilePage updateContent = {this.handleClick}/> }
+        {choice === "loginScreen" &&
+          [<LoginPage updateContent={this.handleClick} key="keyLoginOverlayOnLandingContent"/>,
+          <CHNavigator updateContent={this.handleClick} signedIn={false} key="keyNavigatorLoginOverlayOnLandingContent"/>,
+          <div className="container-landing my-content-landing" key="keyContentLoginOverlayOnLandingContent">
+            <CHLandingContent />
+          </div>,
+          <CHFooter key="keyFooterLoginOverlayOnLandingContent"/>]
+        }
+
+        {choice === "signupScreen" &&
+          [<SignupPage updateContent={this.handleClick} key="keySignUpOverlayOnLandingContent"/>,
+          <CHNavigator updateContent={this.handleClick} signedIn={false} key="keyNavigatorSignUpOverlayOnLandingContent"/>,
+          <div className="container-landing my-content-landing" key="keyContentSignUpOverlayOnLandingContent">
+            <CHLandingContent />
+          </div>,
+          <CHFooter key="keyFooterSignUpOverlayOnLandingContent"/>]
+        }
+
+        {choice === "homeSignedIn" &&
+          [<CHNavigator updateContent={this.handleClick} signedIn={true} firstName={optional1} key="keyNavigatorLandingContent"/>,
+          <div className="container-landing my-content-landing"  key="keyLandingContent">
+            <CHLandingContent />
+          </div>,
+          <CHFooter key="keyFooterLandingContent"/>]
+        }
+
+        {choice === "profile" &&
+        [<CHNavigator updateContent={this.handleClick} signedIn={true} firstName={optional1} key="keyNavigatorLandingContent"/>,
+            <div className="profile-nav" key="profilenavbar">
+              <ProfileNavigator />
+            </div>,
+          <div className="profile-page"  key="profilecontent">
+            <ProfilePage />
+          </div>,
+          <CHFooter key="keyFooterLandingContent"/>]
+        }
       </div>
     );
   }
