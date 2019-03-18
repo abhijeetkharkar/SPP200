b4a0b5edb1d8cbe2d2f8fc4ecd75495bccffbbc7
abhijeetkharkar <abhijeetkharkar@gmail.com>
2019-03-17 21:38:29 -0500

    Unit testing completed for the changes. Fixes #132. Fixes #134

--- frontend/course-hub-ui/src/tests/CHNavigator.test.js
+++ frontend/course-hub-ui/src/tests/CHNavigator.test.js
@@ -1,12 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import renderer from 'react-test-renderer'
 import CHNavigator from '../js/CHNavigator';
-import Footer from '../js/CHFooter'
 import { shallow } from 'enzyme';
-import { render } from 'enzyme';
-import { mount } from 'enzyme';
-import PropTypes from 'prop-types';
 const firebase = require('../FirebaseUtils');
 
 const fetch = require('node-fetch');
@@ -69,4 +63,84 @@ test('Testing On-click feature', async () => {
     const instance = wrapper.instance();
     wrapper.find('#loginButtonNavigator').simulate("click");
     expect(handleClick.mock.calls.length).toBe(1);
+});
+
+
+
+test('Testing onchange autocomplete fetch - Happy 1', async () => {
+    const handleClick = jest.fn();
+    const wrapper = shallow(<CHNavigator  updateContent={handleClick}/>);
+    const instance = wrapper.instance();
+    const event = {
+        preventDefault() { },
+        target: { value: 'deep' }
+    };
+    wrapper.setState({ searchString: "abc", suggestions: ["abc", "def"] });
+    const response = { suggestions: ["Deep Learning", "Machine Learning"] };
+    fetch.mockResponseOnce(JSON.stringify(response));
+    instance.handleSearchStringChange(event);
+    expect(instance.state.suggestions[0]).toBe("abc"); // bad
+});
+
+test('Testing onchange autocomplete fetch - Happy 2', async () => {
+    const handleClick = jest.fn();
+    const wrapper = shallow(<CHNavigator  updateContent={handleClick}/>);
+    const instance = wrapper.instance();
+    const event = {
+        preventDefault() { },
+        target: { value: 'deep' }
+    };
+    wrapper.setState({ searchString: "ab", suggestions: ["abc", "def"] });
+    const response = { suggestions: ["Deep Learning", "Machine Learning"] };
+    fetch.mockResponseOnce(JSON.stringify(response));
+    instance.handleSearchStringChange(event);
+});
+
+test('Testing onchange autocomplete fetch - Sad', async () => {
+    const handleClick = jest.fn();
+    const wrapper = shallow(<CHNavigator updateContent={handleClick}/>);
+    const instance = wrapper.instance();
+    const event = {
+        preventDefault() { },
+        target: { value: 'deep' }
+    };
+    wrapper.setState({ searchString: "abc", suggestions: ["abc", "def"] });
+    const response = { suggestions: ["Deep Learning", "Machine Learning"] };
+    fetch.mockResponseOnce(response);
+    instance.handleSearchStringChange(event);
+});
+
+test('Testing handleOnclick', async () => {
+    const handleClick = jest.fn();
+    const wrapper = shallow(<CHNavigator updateContent={handleClick} />);
+    wrapper.setState({ searchString: "abc", suggestions: ["abc", "def"] });
+    const instance = wrapper.instance();
+    const event = {
+        preventDefault() { },
+        target: { innerText: 'deep' }
+    };
+    instance.handleOnclick(event);
+    expect(instance.state.showResults).toBe(false);
+});
+
+test('Testing handleSearch', async () => {
+    const handleClick = jest.fn();
+    const handlePagination = jest.fn();
+    const wrapper = shallow(<CHNavigator updateContent={handleClick} updatePage={handlePagination}/>);
+    wrapper.setState({ searchString: "abc", suggestions: ["abc", "def"] });
+    const instance = wrapper.instance();
+    const event = {
+        preventDefault() { },
+        target: { innerText: 'deep' }
+    };
+    instance.handleSearch(event);
+});
+
+test('Testing search results componentWillReceiveProps', async () => {
+    const handleClick = jest.fn();
+    const handlePagination = jest.fn();
+	const wrapper = shallow(<CHNavigator updateContent={handleClick} updatePage={handlePagination} firstName={"Test1"} email={"test1@test.com"} searchString={"test"} pageNumber={1}/>);
+	const instance = wrapper.instance();
+    instance.componentWillReceiveProps();
+	expect(wrapper.exists()).toBe(true);
 });
