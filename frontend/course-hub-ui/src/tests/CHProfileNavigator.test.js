d3fa294d35d662f4db0af380a8ab0a2f8ce060c5
Ali Asgher <ali-asgher@uiowa.edu>
2019-04-01 00:45:16 -0500

    Added test for deleting user account - exception

--- frontend/course-hub-ui/src/tests/CHProfileNavigator.test.js
+++ frontend/course-hub-ui/src/tests/CHProfileNavigator.test.js
@@ -1,8 +1,52 @@
 import React from 'react';
 import CHProfileNavigator from '../js/CHProfileNavigator';
-import { shallow } from 'enzyme';
+import {configure, shallow} from 'enzyme';
+import Adapter from "enzyme-adapter-react-16/build";
 
-test('Testing Loading of Profile Navigator webpage', () => {
-    const wrapper = shallow(<CHProfileNavigator />);
-    expect(wrapper.exists()).toBe(true);
+configure({ adapter: new Adapter() })
+
+describe('Testing Profile Navigator', () => {
+    test('Testing Loading of Profile Navigator webpage', () => {
+        const wrapper = shallow(<CHProfileNavigator />, {disableLifecycleMethods: true});
+
+        expect(wrapper.exists()).toBe(true);
+    });
+
+    test('Testing edit profile nav item', async () => {
+        const handleClick = jest.fn();
+
+        const wrapper = shallow(<CHProfileNavigator />, {disableLifecycleMethods: true});
+
+        const instance = wrapper.instance();
+        instance.handleNavItemChange(1);
+
+        expect(instance.state.edit_profile_tag.style.display).toBe("Block");
+
+    });
+
+    test('Testing reviews nav item', async () => {
+        const handleClick = jest.fn();
+
+        // const profile_wrapper = shallow(<ProfilePage updateContent={handleClick}/>);
+        const wrapper = shallow(<CHProfileNavigator />, {disableLifecycleMethods: true});
+
+        const instance = wrapper.instance();
+        instance.handleNavItemChange(2);
+
+        expect(instance.state.reviews_tag.style.display).toBe("Block");
+
+    });
+
+    test('Testing courses nav item', async () => {
+        const handleClick = jest.fn();
+
+        // const profile_wrapper = shallow(<ProfilePage updateContent={handleClick}/>);
+        const wrapper = shallow(<CHProfileNavigator />, {disableLifecycleMethods: true});
+
+        const instance = wrapper.instance();
+        instance.handleNavItemChange(3);
+
+        expect(instance.state.courses_tag.style.display).toBe("Block");
+
+    });
 });
