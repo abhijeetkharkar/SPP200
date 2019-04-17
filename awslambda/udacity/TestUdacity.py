5fb6c872348f5509b67756079cc9700f9a407083
Indranil Guha <indranil-guha@uiowa.edu>
2019-04-16 23:01:05 -0500

    backend for microdegree fixes #171

--- awslambda/udacity/TestUdacity.py
+++ awslambda/udacity/TestUdacity.py
@@ -24,20 +24,20 @@ class Testudacity(TestCase):
 
     @requests_mock.mock()
     def test_valid_url(self,req):
-        req.get('https://www.udacity.com/public-api/v0/courses', text='{"courses":[{}]}')
+        req.get('https://www.udacity.com/public-api/v1/courses', text='{"courses":[{}]}')
         with patch("json.loads",MagicMock('{cool}')) as m:
             fetch_records_udacity("info.txt")
             m.called
 
     @requests_mock.mock()
     def test_api_endpoint_failure(self, req):
-        req.get('https://www.udacity.com/public-api/v0/courses', text='{"courses":[{}],"status_code": 404}')
+        req.get('https://www.udacity.com/public-api/v1/courses', text='{"courses":[{}],"status_code": 404}')
         with patch("json.loads", return_value={"courses":[]}):
             fetch_records_udacity("info.txt")
 
     @requests_mock.mock()
     def test_response_parsed_properly(self, req):
-        req.get('https://www.udacity.com/public-api/v0/courses', text='{"courses":[{}]}')
+        req.get('https://www.udacity.com/public-api/v1/courses', text='{"courses":[{}]}')
         with patch("awslambda.udacity.loader.parse_json",return_value={"CourseId":{}}):
             with patch("awslambda.udacity.loader.search_elastic_server",MagicMock('{"CourseId":{}}')) as m:
                 fetch_records_udacity("info.txt")
@@ -45,14 +45,14 @@ class Testudacity(TestCase):
 
     @requests_mock.mock()
     def test_record_found_in_database(self, req):
-        req.get('https://www.udacity.com/public-api/v0/courses', text='{"courses":[{}]}')
+        req.get('https://www.udacity.com/public-api/v1/courses', text='{"courses":[{}]}')
         with patch("awslambda.udacity.loader.parse_json", return_value={"CourseId": "testid123"}):
             with patch("awslambda.udacity.loader.search_elastic_server", return_value=True):
                 fetch_records_udacity("info.txt")
 
     @requests_mock.mock()
     def test_record_not_found_database(self, req):
-        req.get('https://www.udacity.com/public-api/v0/courses', text='{"courses":[{}]}')
+        req.get('https://www.udacity.com/public-api/v1/courses', text='{"courses":[{}]}')
         with patch("awslambda.udacity.loader.parse_json", return_value={"CourseId": "testid123"}):
             with patch("awslambda.udacity.loader.search_elastic_server", return_value=True):
                 with patch("awslambda.udacity.loader.add_data_elastic_search",MagicMock({"CourseId": "testid123"})) as m:
