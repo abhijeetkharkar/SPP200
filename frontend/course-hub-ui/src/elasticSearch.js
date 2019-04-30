1d0969cd4c3ee93b82f2a3e4e0d22b4d60dc0708
Ali Asgher <ali-asgher@uiowa.edu>
2019-04-30 00:27:25 -0500

    Created elastic search query function to fetch user microdegrees data

--- frontend/course-hub-ui/src/elasticSearch.js
+++ frontend/course-hub-ui/src/elasticSearch.js
@@ -425,8 +425,21 @@ const saveMicroDegree = async (payload) => {
     });
 }
 
+const getUserMicroDegree = async (payload) => {
+    return await fetch(process.env.REACT_APP_GET_USER_MICRODEGREE, {
+        method: 'POST',
+        body: JSON.stringify(payload),
+        headers: { 'Content-Type': 'application/json' }
+    }).then(response => {
+        console.log("Fetching microdegree", JSON.stringify(payload));
+        console.log(response);
+        return response.json();
+    });
+
+}
+
 export {addUser, searchUser, getUserDetails, updateUser, elasticDeleteUser, addDeal, 
     addReview, getReviews, updateReview, getCourseDetails, updateCourseRating, 
     getDealsfromES, getSpecificDealFromES, getDealVotesFromES, updateDealVotesinES, 
     updateDealsinES, addDealVoteinES, addUserReviewLike, getUserReviewLikes, updateUserReviewLike,
-    getMicroDegreeSuggestions, saveMicroDegree };
+    getMicroDegreeSuggestions, saveMicroDegree, getUserMicroDegree };
